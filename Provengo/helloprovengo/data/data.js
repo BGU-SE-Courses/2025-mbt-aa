b3a2bf23d5d2df1da85284d0133379f36fc079bd
Eden Nahum <102467189+edennahum@users.noreply.github.com>
2025-01-27 00:15:18 +0200

    Update data.js

--- Provengo/helloprovengo/data/data.js
+++ Provengo/helloprovengo/data/data.js
@@ -1,15 +1,49 @@
+/* @provengo summon selenium */
+
 /*
  *  This is a good place to put common test data, project-wide constants, etc.
  */
 
-const URL = 'https://www.google.com/?hl=en';
+const URL = 'http://localhost/';
+const studentUsername = 'student'
+const studentPassword = 'Student123!'
+const teacherUsername = 'teacher'
+const teacherPassword = 'Teacher123!'
 
 const xpaths = {
-  searchWindow: {
-    searchInput: '//textarea[@aria-label="Search"]',
-    searchButton: '//input[@aria-label="Google Search"]',
-    feelingLuckyButton: '//input[@aria-label="I\'m Feeling Lucky"]'
+  loginWindowButton: "",
+
+  loginWindow:{
+    usernameInput: "",
+    passwordInput: "",
+    loginButton: "",
+  },
+
+  studentWindow:{
+    studentCoursesButton: "",
+    studentTestCourseButton: "",
+    studentForumButton: "",
+    DiscussionSubscriptionButton: "",
+  },
+
+  teacherWindow:{
+    teacherCoursesButton: "",
+    teacherTestCourseButton: "",
+    teacherForumButton: "",
+    deleteDiscussionFirstButton: "",
+    deleteDiscussionSeconedButton: "",
   }
+
 }
 
-const searchTerm = 'pizza'
+// const URL = 'https://www.google.com/?hl=en';
+
+// const xpaths = {
+//   searchWindow: {
+//     searchInput: '//textarea[@aria-label="Search"]',
+//     searchButton: '//input[@aria-label="Google Search"]',
+//     feelingLuckyButton: '//input[@aria-label="I\'m Feeling Lucky"]'
+//   }
+// }
+
+// const searchTerm = 'pizza'
