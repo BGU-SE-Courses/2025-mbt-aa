14f2a05fc29cdd00747fc1ea56fe52f89bff1bf5
Eden Nahum <102467189+edennahum@users.noreply.github.com>
2025-01-28 21:42:51 +0200

    Update data.js

--- Provengo/moodle/data/data.js
+++ Provengo/moodle/data/data.js
@@ -4,47 +4,47 @@
  *  This is a good place to put common test data, project-wide constants, etc.
  */
 
-// const URL = 'http://localhost/';
-// const studentUsername = 'student'
-// const studentPassword = 'Student123!'
-// const teacherUsername = 'teacher'
-// const teacherPassword = 'Teacher123!'
-//
-// const xpaths = {
-//   loginWindowButton: "//*[@id="usernavigation"]/div/div/span/a",
-//
-//   loginWindow:{
-//     usernameInput: "//div[1]/form[1]/div[1]/input[1]",
-//     passwordInput: "//div[2]/div[1]/input[1]",
-//     loginButton: "//form[1]/div[3]/button[1]",
-//   },
-//
-//   studentWindow:{
-//     studentCoursesButton: "/html/body/div[2]/nav/div/div[1]/nav/ul/li[3]/a",
-//     studentTestCourseButton: "/html/body/div[2]/div[3]/div/div[2]/div/section/div/aside/section/div/div/div[1]/div[2]/div/div/div[1]/div/div/div/div/div[1]/div/div/a/span[3]/span[2]",
-//     studentForumButton: "/html/body/div[2]/div[4]/div/div[3]/div/section/div/div/div/ul/li[3]/div/div[2]/ul/li/div/div[2]/div[2]/div/div/a",
-//     DiscussionSubscriptionButton: "/html/body/div[3]/div[4]/div/div[2]/div/section/div[2]/div[2]/div[3]/div/table/tbody/tr/td[5]/div",
-//   },
-//
-//   teacherWindow:{
-//     teacherCoursesButton: "/html/body/div[2]/nav/div/div[1]/nav/ul/li[3]/a",
-//     teacherTestCourseButton: "/html/body/div[2]/div[3]/div/div[2]/div/section/div/aside/section/div/div/div[1]/div[2]/div/div/div[1]/div/div/div/div/div[1]/div/div/a/span[3]/span[2]",
-//     teacherForumButton: "/html/body/div[2]/div[4]/div/div[3]/div/section/div/div/div/ul/li[3]/div/div[2]/ul/li/div/div[2]/div[2]/div/div/a",
-//     teacherEnterDiscussion: "/html/body/div[3]/div[4]/div/div[3]/div/section/div[2]/div[2]/div[3]/div/table/tbody/tr/th/div/div[1]/a",
-//     teacherDeleteDiscussion: "/html/body/div[2]/div[4]/div/div[3]/div/section/div[2]/div/article/div[1]/div/div/div[2]/div[2]/div/a[3]",
-//     teacherConfirmDelete: "/html/body/div[2]/div[4]/div/div[3]/div/section/div[2]/div/div/div[3]/div/div[2]/form/button"
-//   }
-//
-// }
-
-const URL = 'https://www.google.com/?hl=en';
+const URL = "http://localhost:8888/moodle405/";
+const studentUsername = 'student'
+const studentPassword = 'Student123!'
+const teacherUsername = 'teacher'
+const teacherPassword = 'Teacher123!'
 
 const xpaths = {
-  searchWindow: {
-    searchInput: '//textarea[@aria-label="Search"]',
-    searchButton: '//input[@aria-label="Google Search"]',
-    feelingLuckyButton: '//input[@aria-label="I\'m Feeling Lucky"]'
+  loginWindowButton: "//*[@id=\"usernavigation\"]/div/div/span/a",
+
+  loginWindow:{
+    usernameInput: "//div[1]/form[1]/div[1]/input[1]",
+    passwordInput: "//div[2]/div[1]/input[1]",
+    loginButton: "//form[1]/div[3]/button[1]",
+  },
+
+  studentWindow:{
+    studentCoursesButton: "/html/body/div[2]/nav/div/div[1]/nav/ul/li[3]/a",
+    studentTestCourseButton: "/html/body/div[2]/div[3]/div/div[2]/div/section/div/aside/section/div/div/div[1]/div[2]/div/div/div[1]/div/div/div/div/div[1]/div/div/a/span[3]/span[2]",
+    studentForumButton: "/html/body/div[2]/div[4]/div/div[3]/div/section/div/div/div/ul/li[3]/div/div[2]/ul/li/div/div[2]/div[2]/div/div/a",
+    DiscussionSubscriptionButton: "/html/body/div[3]/div[4]/div/div[2]/div/section/div[2]/div[2]/div[3]/div/table/tbody/tr/td[5]/div",
+  },
+
+  teacherWindow:{
+    teacherCoursesButton: "/html/body/div[2]/nav/div/div[1]/nav/ul/li[3]/a",
+    teacherTestCourseButton: "/html/body/div[2]/div[3]/div/div[2]/div/section/div/aside/section/div/div/div[1]/div[2]/div/div/div[1]/div/div/div/div/div[1]/div/div/a/span[3]/span[2]",
+    teacherForumButton: "/html/body/div[2]/div[4]/div/div[3]/div/section/div/div/div/ul/li[3]/div/div[2]/ul/li/div/div[2]/div[2]/div/div/a",
+    teacherEnterDiscussion: "/html/body/div[3]/div[4]/div/div[3]/div/section/div[2]/div[2]/div[3]/div/table/tbody/tr/th/div/div[1]/a",
+    teacherDeleteDiscussion: "/html/body/div[2]/div[4]/div/div[3]/div/section/div[2]/div/article/div[1]/div/div/div[2]/div[2]/div/a[3]",
+    teacherConfirmDelete: "/html/body/div[2]/div[4]/div/div[3]/div/section/div[2]/div/div/div[3]/div/div[2]/form/button"
   }
+
 }
 
-const searchTerm = 'pizza'
+// const URL = 'https://www.google.com/?hl=en';
+
+// const xpaths = {
+//   searchWindow: {
+//     searchInput: '//textarea[@aria-label="Search"]',
+//     searchButton: '//input[@aria-label="Google Search"]',
+//     feelingLuckyButton: '//input[@aria-label="I\'m Feeling Lucky"]'
+//   }
+// }
+
+// const searchTerm = 'pizza'
